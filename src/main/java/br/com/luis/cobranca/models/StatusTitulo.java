bbd678d125bbec24b0dc18e98f0ef6bee1b81b2b
Luistf <luistf.book@gmail.com>
2017-12-17 18:23:07 -0200

    criando projeto spring boot, adicionando bootstrap criando a logica de salvar

---
+++ src/main/java/br/com/luis/cobranca/models/StatusTitulo.java
@@ -0,0 +1,16 @@
+package br.com.luis.cobranca.models;
+
+public enum StatusTitulo {
+
+	PENDENTE("Pendente"), RECEBIDO("Recebido");
+	
+	private String descricao;
+	
+	private StatusTitulo(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+}
